8335ceb79811b9832be52ef8f53d1dde814d77a9
Loly <lorena.lgodoy@gmail.com>
2019-06-01 19:46:21 -0300

    Se valida pass

--- proyecto-limpio-spring/src/main/java/ar/edu/unlam/tallerweb1/servicios/ServicioRegistrarUsuarioImpl.java
+++ proyecto-limpio-spring/src/main/java/ar/edu/unlam/tallerweb1/servicios/ServicioRegistrarUsuarioImpl.java
@@ -1,7 +1,8 @@
 package ar.edu.unlam.tallerweb1.servicios;
 
-import java.util.ArrayList;
-
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import javax.inject.Inject;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,9 +15,34 @@ public class ServicioRegistrarUsuarioImpl implements ServicioRegistrarUsuario {
 	
 	@Inject
 	private RegistrarUsuarioDao servicioRegitrarUsuarioDao;
-
+	
 	@Override
 	public boolean registrarUsuario(Usuario usuario) {
-		return servicioRegitrarUsuarioDao.registrarUsuario(usuario);
+				
+		//Indico localización del txt a comparar
+		File commonPass = new File("c:/pass10000.txt");
+		String pass = usuario.getPassword();
+		if(pass.length()<12){
+			return false;
+		} else if(pass.length()>=12){
+			try{
+				FileReader fr = new FileReader(commonPass); 
+				BufferedReader br = new BufferedReader(fr);
+				
+				String linea = br.readLine();
+				
+				while(linea != null) {
+				    linea = br.readLine();
+				  }
+				br.close();
+				 
+				}catch(Exception e) {
+					  return false;
+				}
+				return true;
+			}else{
+				servicioRegitrarUsuarioDao.registrarUsuario(usuario);
+				return true;
+			}
 	}
 }
